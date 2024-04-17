8bceaa08d4fb30f10089ea85388369a956699419
Aliyev-Rufat <Rufat0747@gmail.com>
2024-04-18 02:25:47 +0400

    Generics , Collections

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            myList<string> seherler = new myList<string>();
+            Console.WriteLine(seherler.count);
+            seherler.Add("Baku");
+        }
+
+        class myList<T>
+        {
+            T[] _array;
+            T[] _tempArray;
+            public myList()
+            {
+                _array = new T[0];
+            }
+            public void Add(T item)
+            {
+                _tempArray = _array;
+                _array = new T[_array.Length + 1];
+                for (int i = 0; i < _array.Length; i++)
+                {
+                    _array[i] = _tempArray[i];
+                }
+                _array[_array.Length - 1] = item;
+            }
+
+            private int _count;
+
+            public int count
+            {
+                get { return _count; }
+            }
+        }
+    }
+}
+
